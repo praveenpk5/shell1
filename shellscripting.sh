7854f75cdd2ef92b930ba6922559d7516e01cbbc
praveenpk5 <maddupraveenkumar5@gmail.com>
2021-09-04 06:37:58 +0000

    allowe changes

--- shellscripting.sh
+++ shellscripting.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #new line is added
-
-echo "hello everyone"
-echo "please provide a value:\c "
+ echo "hello everyone"
+ echo "please provide a value:\c "
 read -r c
-i= 1
-while [$i -le 10]
+i=1
+while '[$i -le 10]'
 do
 b='expr $c \* $i'
-echo "$c * $i= $b"
+ echo "$c * $i= $b"
 i='expr $i+1'
-done
