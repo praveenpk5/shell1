023c11a158117f907aaad3fc6bc4289d7ea82db0
praveenpk5 <maddupraveenkumar5@gmail.com>
2021-09-07 10:42:04 +0000

    commit the changes

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,4 @@
-echo "setting MongoDB"
+echo "Setting MongoDB"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -7,10 +7,10 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
-echo "installing MongoDB"
+echo "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
 
-echo "config MongoDB"
+echo "Config MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  
 
 
@@ -18,12 +18,12 @@ echo "starting MongoDB"
  systemctl enable mongod
  systemctl restart mongod
 
-echo "downloading MongoDB"
+echo "Downloading MongoDB"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
 unzip -o mongodb.zip &>>/tmp/log
 cd mongodb-main 
-echo "loading schema"
+echo "Loading schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
