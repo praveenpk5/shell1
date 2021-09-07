d7507766e6c8e7f34af15ac6e7b94d5f97ce95b8
praveenpk5 <maddupraveenkumar5@gmail.com>
2021-09-07 16:01:21 +0000

    commit the changes

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-status_check() {
- if [ $? -eq 0 ]; then
- echo -e "\e[32mSUCCESS\e[0m"
- else
- echo -e "\e[31mFAILURE\e[0m"
- exit 2
- fi
- } 
-print() {
- echo -n -e "$1 \t- "
+
+Status_Check() {
+  if [ $1 -eq 0 ]; then 
+    echo -e "\e[32mSUCCESS\e[0m"
+  else 
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi 
 }
+
+Print() {
+  echo -e "\n\t\t\e[36m----------------- $1 ----------------------\e[0m\n" >>$LOG
+  echo -n -e "$1 \t- "
+}
+
+if [ $UID -ne 0 ]; then 
+  echo -e "\n\e[1;33mYou should execute this script as root User\e[0m\n"
+  exit 1
+fi 
+
+LOG=/tmp/roboshop.log 
+rm -f $LOG
