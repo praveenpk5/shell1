5eb3fd672fe55f0bcf1887a5271ad4b339ad3209
praveenpk5 <maddupraveenkumar5@gmail.com>
2021-09-07 15:50:30 +0000

    commit the changes

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-status_check() {
- if [ $? -eq 0 ]; then
- echo -e "\e[32mSUCCESS\e[0m"
- else
- echo -e "\e[31mFAILURE\e[0m"
- exit 2
- fi
- } 
-print() {
- echo -n -e "$1 \t- "
-}
- 
+source components/common.sh
+
 print "Setting MongoDB"
 
 echo '[mongodb-org-4.2]
