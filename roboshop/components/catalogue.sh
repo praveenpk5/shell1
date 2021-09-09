d69158ab943d65ea6e8fbdf5c57e3d2c57fe8abc
praveenpk5 <maddupraveenkumar5@gmail.com>
2021-09-09 13:06:33 +0000

    adding

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -7,20 +7,29 @@ yum install nodejs make gcc-c++ -y &>>$LOG
 status_check $?
 
 print "Adding Roboshop User"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+ echo "user already there, so skipping" &>>$LOG
+ else
 useradd roboshop &>>$LOG
+fi
+status_check $?
 
 print "Downloading catalogue content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 status_check $?
 
+print "extracting catalogue"
 cd /home/roboshop
 unzip /tmp/catalogue.zip &>>$LOG
 mv catalogue-main catalogue
 status_check $?
 
+print "Download NodeJS Dependencies"
 cd /home/roboshop/catalogue
 npm install &>>$LOG
 
+chown roboshop:roboshop -R /home/roboshop
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
